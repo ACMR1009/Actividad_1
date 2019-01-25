2af7df2cf050dc21f9382aaa9888995a9a2ce517
ACMR1009 <anacristina.munguia@gmail.com>
2019-01-25 08:23:03 -0700

    Initial commit

---
+++ app/src/main/java/actividad1/acmr/MainActivity.java
@@ -0,0 +1,13 @@
+package actividad1.acmr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
